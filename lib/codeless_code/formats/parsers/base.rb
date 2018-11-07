e922b69e4845750c016c6ebbbaa7f4b616221e20
Jon Sangster <jon@ertt.ca>
2018-11-06 21:58:52 -0800

    Dynamically create abstract methods

--- lib/codeless_code/formats/parsers/base.rb
+++ lib/codeless_code/formats/parsers/base.rb
@@ -19,139 +19,80 @@ module CodelessCode
   module Formats
     module Parsers
       class Base < MediaWikiWalker
-          def initialize(ctx)
-            @ctx = ctx
+        # Reimplement these
+        #
+        # @example
+        #   def parse_paragraph(ast)
+        #     parse_wiki_ast(ast)
+        #   end
+        ABSTRACT_METHODS = %i[
+          parse_paragraph
+          parse_paste
+          parse_formatted
+          parse_list_item
+          parse_list_term
+          parse_list_definition
+          parse_preformatted
+          parse_section
+          parse_link
+          parse_internal_link
+          parse_internal_link_item
+          parse_table
+          parse_table_row
+          parse_table_cell
+          parse_element
+          parse_template
+          parse_category
+          parse_keyword
+        ].freeze
+
+        def initialize(ctx)
+          @ctx = ctx
+        end
+
+        protected
+
+        def parse_wiki_ast(ast)
+          super(ast).join
+        end
+
+        ABSTRACT_METHODS.each do |meth|
+          define_method(meth) { |ast| parse_wiki_ast(ast) }
+        end
+
+        # Reimplement this
+        def parse_text(ast)
+          ast.contents
+        end
+
+        # Reimplement this
+        def parse_list(ast)
+          ast.children.map(&method(:parse_list_child))
+        end
+
+        # Reimplement this
+        def parse_resource_link(ast)
+          ast.children.map do |c|
+            parse_internal_link_item(c) if c.is_a?(InternalLinkItemAST)
           end
+        end
 
-          protected
-
-          def parse_wiki_ast(ast)
-            super(ast).join
+        # Reimplement this
+        def parse_category_link(ast)
+          ast.children.map do |c|
+            parse_internal_link_item(c) if c.is_a?(InternalLinkItemAST)
           end
+        end
 
-          #Reimplement this
-          def parse_paragraph(ast)
-            parse_wiki_ast(ast)
+        private
+
+        def parse_list_child(c)
+          case c
+          when ListItemAST       then parse_list_item(c)
+          when ListTermAST       then parse_list_term(c)
+          when ListDefinitionAST then parse_list_definition(c)
           end
-
-          #Reimplement this
-          def parse_paste(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_formatted(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_text(ast)
-            ast.contents
-          end
-
-          #Reimplement this
-          def parse_list(ast)
-            ast.children.map(&method(:parse_list_child))
-          end
-
-          #Reimplement this
-          def parse_list_item(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_list_term(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_list_definition(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_preformatted(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_section(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_link(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_internal_link(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_resource_link(ast)
-            ast.children.map do |c|
-              parse_internal_link_item(c) if c.is_a?(InternalLinkItemAST)
-            end
-          end
-
-          #Reimplement this
-          def parse_category_link(ast)
-            ast.children.map do |c|
-              parse_internal_link_item(c) if c.is_a?(InternalLinkItemAST)
-            end
-          end
-
-          #Reimplement this
-          def parse_internal_link_item(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_table(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_table_row(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_table_cell(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_element(ast)
-            parse_wiki_ast(ast)
-          end
-
-          #Reimplement this
-          def parse_template(ast)
-            ast.template_name
-          end
-
-          #Reimplement this
-          def parse_category(ast)
-            raise NotImplementedError
-          end
-
-          #Reimplement this
-          def parse_keyword(ast)
-            parse_wiki_ast(ast)
-          end
-
-          private
-
-          def parse_list_child(c)
-            case c
-            when ListItemAST       then parse_list_item(c)
-            when ListTermAST       then parse_list_term(c)
-            when ListDefinitionAST then parse_list_definition(c)
-            end
-          end
+        end
       end
     end
   end
