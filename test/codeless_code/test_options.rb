1b6c5d02ec6ca25f906d3adf470fc025bc9de7ba
Jon Sangster <jon@ertt.ca>
2018-11-09 04:02:32 -0800

    require "slop"

--- test/codeless_code/test_options.rb
+++ test/codeless_code/test_options.rb
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
 require 'helper'
+require 'slop'
 
 class TestOptions < UnitTest
   def test_opts
